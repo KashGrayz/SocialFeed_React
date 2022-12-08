ec8551f195ebfb1c7cf02f4f7ff6d79292af3c11
Kash Grays <kasharsgrays93@.com>
2022-12-08 01:08:18 -0500

    Refactored App.css

--- src/Components/UserPosts/UserPosts.jsx
+++ src/Components/UserPosts/UserPosts.jsx
@@ -5,9 +5,12 @@ const UserPosts = (props) => {
     return (
         <div>
             <div>
+                <br></br>
                 <h3>{props.posts.name}</h3>
+                <br></br>
                 <p>{props.posts.comment}</p>
                 <PostButton/>
+                <hr style={{'height':'2px','border-width':'0','color':'gray','background-color':'gray'}}></hr>
             </div>
         </div>
     );
